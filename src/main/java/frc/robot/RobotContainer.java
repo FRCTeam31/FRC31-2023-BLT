2bd2dd315eac916298b48abb7104bcd8c5bb1ea2
zeroClearAmerican <emperor@emperorof.space>
2023-02-11 08:57:56 -0600

    Initial commit -- migrated from test bench

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,71 @@
+package frc.robot;
+
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.config.DriveMap;
+
+public class RobotContainer {
+    public CommandJoystick mController;
+    public SwerveModule mFrontLeftSwerve;
+    public SwerveModule mFrontRightSwerve;
+    public SwerveModule mRearLeftSwerve;
+    public SwerveModule mRearRightSwerve;
+    public Drivetrain mDrivetrain;
+
+    public RobotContainer() {
+        mFrontLeftSwerve = new SwerveModule(
+            DriveMap.kFrontLeftDrivingMotorId, 
+            DriveMap.kFrontLeftSteeringMotorId, 
+            DriveMap.kFrontLeftEncoderAIOChannel,
+            DriveMap.kFrontLeftEncoderOffset);
+
+        mFrontRightSwerve = new SwerveModule(
+            DriveMap.kFrontRightDrivingMotorId, 
+            DriveMap.kFrontRightSteeringMotorId, 
+            DriveMap.kFrontRightEncoderAIOChannel, 
+            DriveMap.kFrontRightEncoderOffset);
+
+        mRearLeftSwerve = new SwerveModule(
+            DriveMap.kRearLeftDrivingMotorId, 
+            DriveMap.kRearLeftSteeringMotorId, 
+            DriveMap.kRearLeftEncoderAIOChannel, 
+            DriveMap.kRearLeftEncoderOffset);
+
+        mRearRightSwerve = new SwerveModule(
+            DriveMap.kRearRightDrivingMotorId, 
+            DriveMap.kRearRightSteeringMotorId, 
+            DriveMap.kRearRightEncoderAIOChannel, 
+            DriveMap.kRearRightEncoderOffset);
+        
+        mDrivetrain = new Drivetrain(mFrontLeftSwerve, mFrontRightSwerve, mRearLeftSwerve, mRearRightSwerve);
+
+        configureBindings();
+    }
+
+    private void configureBindings() {
+        mController = new CommandJoystick(0);
+
+        // Default commands
+        mDrivetrain.setDefaultCommand(Commands.run(() -> {
+            // Grab the X and Y axis from the left joystick on the controller
+            var strafeX = mController.getRawAxis(0);
+            var forwardY = -mController.getRawAxis(1);
+
+            // Right trigger should rotate the robot clockwise, left counterclockwise
+            // Add the two [0,1] trigger axes together for a combined period of [-1, 1]
+            var rotation = mController.getRawAxis(2) + -mController.getRawAxis(3);
+
+            mDrivetrain.drive(strafeX, forwardY, rotation, true);
+        }, mDrivetrain, mFrontLeftSwerve, mFrontRightSwerve, mRearLeftSwerve, mRearRightSwerve));
+
+        // Button bindings
+        mController.button(3).onTrue(Commands.runOnce(() -> mDrivetrain.resetGyro()));
+    }
+
+    public Command getAutonomousCommand() {
+        return new InstantCommand();
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.DriveMap;
+import frc.robot.sensors.navx.AHRS;
+import frc.robot.utilities.EventLogger;
+
+public class Drivetrain extends SubsystemBase {
+  // Default PID values for steering each module and driving each module
+  private SwerveModule mFrontLeftModule;
+  private SwerveModule mFrontRightModule;
+  private SwerveModule mRearLeftModule;
+  private SwerveModule mRearRightModule;
+
+  // Build a gyro and a kinematics class for our drive
+  final AHRS mGyro = new AHRS(Port.kUSB);
+  final SwerveDriveKinematics mKinematics = new SwerveDriveKinematics(
+    DriveMap.frontLeftLocation, 
+    DriveMap.frontRightLocation, 
+    DriveMap.rearLeftLocation, 
+    DriveMap.rearRightLocation);
+
+  SwerveDriveOdometry mOdometry = new SwerveDriveOdometry(mKinematics, 
+    mGyro.getRotation2d(), 
+    new SwerveModulePosition[]{
+      mFrontLeftModule.getPosition(),
+      mFrontRightModule.getPosition(),
+      mRearLeftModule.getPosition(),
+      mRearRightModule.getPosition(),
+    },
+    new Pose2d(0, 0, Rotation2d.fromDegrees(90)));
+
+  Field2d mField = new Field2d();
+
+  /** Creates a new SwerveDriveTrainSubsystem. */
+  public Drivetrain(SwerveModule flModule, SwerveModule frModule, SwerveModule rlModule, SwerveModule rrModule) {
+    setName("DRIVE");
+    mFrontLeftModule = flModule;
+    mFrontRightModule = frModule;
+    mRearLeftModule = rlModule;
+    mRearRightModule = rrModule;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    var gyroAngle = mGyro.getRotation2d();
+    SmartDashboard.putNumber("Drivetrain gyro angle", gyroAngle.getDegrees());
+
+    var robotPose = mOdometry.update(gyroAngle, new SwerveModulePosition[] {
+      mFrontLeftModule.getPosition(), mFrontRightModule.getPosition(),
+      mRearLeftModule.getPosition(), mRearRightModule.getPosition()
+    });
+
+    mField.setRobotPose(robotPose);
+  }
+
+  public void resetGyro() {
+    mGyro.reset();
+    EventLogger.Information(getName(), "Reset gyro");
+  }
+
+  public void drive(double strafe, double forward, double rotation, boolean fieldRelative) {
+    var desiredChassisSpeeds = fieldRelative
+      ? ChassisSpeeds.fromFieldRelativeSpeeds(strafe, forward, rotation, mGyro.getRotation2d())
+      : new ChassisSpeeds(strafe, forward, rotation);
+
+    var swerveModuleStates = mKinematics.toSwerveModuleStates(desiredChassisSpeeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveMap.kDriveMaxSpeedMetersPerSecond);
+
+    mFrontLeftModule.setDesiredState(swerveModuleStates[0]);
+    mFrontRightModule.setDesiredState(swerveModuleStates[1]);
+    mRearLeftModule.setDesiredState(swerveModuleStates[2]);
+    mRearRightModule.setDesiredState(swerveModuleStates[3]);
+  }
+}
