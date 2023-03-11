ce8cd214598775e0949cd7b87fc4f05756e25dd1
DjAce626 <jacesanders626@gmail.com>
2023-03-11 10:43:46 -0600

    wip

---
+++ src/main/java/frc/robot/config/foram_map.java
@@ -0,0 +1,18 @@
+package frc.robot.config;
+
+import prime.models.PidConstants;
+
+public class foram_map {
+    
+// CAN ids
+public static final int kfoeram1Id = 23;
+public static final int kfoeram2Id = 24;
+public static final int kfoeramId = 20;
+
+// PID
+public static final PidConstants AnglePid = new PidConstants(0.04);
+
+// constants
+public static final double kOpenLoopRampRate = 1.00;
+
+}
---
+++ src/main/java/frc/robot/subsystems/LazyTalonSRX.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class LazyTalonSRX {
+
+    public LazyTalonSRX(int kfoeramid, MotorType kbrushless) {
+    }
+
+    public void restoreFactoryDefaults() {
+    }
+
+    public void clearFaults() {
+    }
+
+    public void setIdleMode(IdleMode kbrake) {
+    }
+
+    public void setOpenLoopRampRate(double kopenloopramprate) {
+    }
+
+}
