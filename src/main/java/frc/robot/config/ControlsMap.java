b1512b64aaf9a685899f90a5f2e4d51a116b9be5
PoE309 <127269850+PoE309@users.noreply.github.com>
2023-03-20 17:06:30 -0500

    Update ControlsMap.java

--- src/main/java/frc/robot/config/ControlsMap.java
+++ src/main/java/frc/robot/config/ControlsMap.java
@@ -7,8 +7,27 @@ package frc.robot.config;
 /** Add your docs here. */
 public class ControlsMap {
 
-
-    // Controller Ports
-    public static final int kDriverPort = 0;
-    public static final int kOperatorPort = 1;
+    //Controllers
+	public static final int DRIVER_PORT = 0;
+	public static final int OPERATOR_PORT = 1;
+		
+	//XboxOne Joysticks
+	public static final int LEFT_STICK_X = 0;
+	public static final int LEFT_STICK_Y = 1;
+	public static final int LEFT_TRIGGER = 2;
+	public static final int RIGHT_TRIGGER = 3;
+	public static final int RIGHT_STICK_X = 4;
+	public static final int RIGHT_STICK_Y = 5;
+	
+	//XboxOne Buttons
+	public static final int A = 1;
+	public static final int B = 2;
+	public static final int X = 3;
+	public static final int Y = 4;
+	public static final int LB = 5;
+	public static final int RB = 6;
+	public static final int LOGO_LEFT = 7;
+	public static final int LOGO_RIGHT = 8;
+	public static final int LEFT_STICK_BUTTON = 9;
+	public static final int RIGHT_STICK_BUTTON = 10;
 }
