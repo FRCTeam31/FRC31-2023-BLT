555eb8c387996be05b5154f553cc9df92cc1b49d
matthew mcconaughey <35619771+zeroClearAmerican@users.noreply.github.com>
2023-03-21 21:58:30 +0000

    Added code for arduino sidecar handling LEDs and ping sensors

--- src/main/java/frc/robot/commands/LightCommands.java
+++ src/main/java/frc/robot/commands/LightCommands.java
@@ -2,10 +2,10 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.Light;
+import frc.robot.subsystems.ArduinoSidecar;
 
 public class LightCommands {
-    public static Command getSetFrontStripColor(Light light, int red, int green, int blue) {
+    public static Command getSetFrontStripColor(ArduinoSidecar light, int red, int green, int blue) {
         return Commands.runOnce(() -> light.setFrontStripColor(red, green, blue), light);
     }
 }
---
+++ src/main/java/frc/robot/subsystems/ArduinoSidecar.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems;
+
+import java.nio.charset.Charset;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArduinoSidecar extends SubsystemBase {
+    private class Map {
+        private static final int ArduinoAddress = 10;
+    }
+
+    private enum StripModes {
+        OFF,
+        SOLID,
+        MODE3,
+        MODE4,
+        MODE5,
+        MODE6,
+        MODE7,
+        MODE8,
+        MODE9,
+    }
+
+    private enum LEDStrips {
+        FRONT,
+        INDICATOR,
+        ACCENT_LEFT,
+        ACCENT_RIGHT
+    }
+
+    private I2C mArduinoI2c;
+    private byte[] mCurrentModes = new byte[] { 0, 0, 0, 0 };
+
+    public ArduinoSidecar() {
+        try {
+            mArduinoI2c = new I2C(Port.kOnboard, Map.ArduinoAddress);
+        } catch (Exception ex) {
+            DriverStation.reportError("[ERROR] Failed to initialize ArduinoSidecar", true);
+        }
+    }
+
+    public void setLEDStripMode(LEDStrips strip, StripModes mode) {
+        var bytesToSend = new byte[mCurrentModes.length];
+        for (int i = 0; i < bytesToSend.length; i++) {
+            bytesToSend[i] = (byte) mCurrentModes[i];
+        }
+
+        if (!mArduinoI2c.writeBulk(bytesToSend, bytesToSend.length))
+            DriverStation.reportError("[ERROR] Aborted while sending LED update to ArduinoSidecar", true);
+    }
+
+    public boolean[] getCliffSensorStates() throws Exception {
+        var readBytes = new byte[4];
+        if (!mArduinoI2c.read(0, 4, readBytes)) 
+            throw new Exception("Aborted while reading ArduinoSidecar cliff sensor data");
+        
+    }
+}
